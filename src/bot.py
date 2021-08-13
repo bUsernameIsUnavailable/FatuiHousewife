0b473a1c0d23e547a4e8885bb1efb88140b5de20
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2021-08-14 02:11:04 +0300

    Add more reaction images

--- src/bot.py
+++ src/bot.py
@@ -57,10 +57,12 @@ media_files = [
         '../media/videos/childe.mp4',
         '../media/videos/chiIde.mp4',
         '../media/videos/persomna.mp4',
-        '../media/images/shapeofyou.gif'
+        '../media/images/shapeofyou.gif',
+        '../media/images/fat_ui.gif'
     ],
     [
-        '../media/images/johnlee.png'
+        '../media/images/johnlee.png',
+        '../media/images/daddy_is_back.png'
     ],
     [
         '../media/images/bornthisway.png'
@@ -79,6 +81,19 @@ media_files = [
         '../media/images/end_user_bonk.gif'
     ]
 ]
+nsfw_files = [
+    [
+        '../media/images/80084.png'
+    ],
+    [
+        '../media/images/80084.png'
+    ],
+    [],
+    [],
+    [],
+    [],
+    []
+]
 
 
 @bot.event
@@ -95,31 +110,34 @@ async def on_message(message: discord.Message):
 
     for i in range(7):
         if any(word in processed_message for word in trigger_words[i]):
-            file = discord.File(determine_file_to_send(media_files[i], i))
+            file = determine_file_to_send(i, message.channel.nsfw)
             if file is not None:
                 try:
-                    await message.channel.send(file=file)
+                    await message.channel.send(file=discord.File(file))
                 except Exception as e:
-                    print(e)
+                    print(f"{message.guild.name}: {e}")
             break
 
 
-def determine_file_to_send(files: list, index: int) -> Optional[str]:
+def determine_file_to_send(index: int, is_nsfw: bool) -> Optional[str]:
     if index == 0:
         if random.randint(0, 3) % 4 == 0:
-            return random.choice(files[2:])
+            return random.choice(media_files[index][2:] + is_nsfw * nsfw_files[index])
 
         now = datetime.now()
-        return files[now.day == birthday.day and now.month == birthday.month]
+        return media_files[index][now.day == birthday.day and now.month == birthday.month]
 
-    if index in [1, 2, 4, 5]:
-        return files[0]
+    if index == 1:
+        return random.choice(media_files[index] + is_nsfw * nsfw_files[index])
+
+    if index in [2, 4, 5]:
+        return media_files[index][0]
 
     if index == 3 and random.randint(0, 9) % 10 == 0:
-        return files[0]
+        return media_files[index][0]
 
     if index == 6:
-        return random.choice(files)
+        return random.choice(media_files[index])
 
     return None
 
